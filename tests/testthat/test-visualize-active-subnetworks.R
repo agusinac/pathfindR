54c794d1b674b28e868ef89a8c43f4bfdac0d255
Ege Ulgen <egeulgen@gmail.com>
2026-06-13 18:14:44 +0100

    Reorganise package structure (#239)
    * Reorganise the R/ directory by moving functions into more intuitively structured scripts and renaming files where appropriate
    * Update tests to mirror the new folder and script structure
    * Regenerate docs
    * Rename fetch_gene_set() utility function to fetch_gene_sets() (extra change)
    * Update the renv env (extra change)
    * Remove the package startup message (extra change)

---
+++ tests/testthat/test-visualize-active-subnetworks.R
@@ -0,0 +1,26 @@
+input_data_frame <- example_pathfindR_input[1:10, c(1, 3)]
+
+example_snw_output <- system.file("extdata", "resultActiveSubnetworkSearch.txt",
+                                  package = "pathfindR")
+example_snws_len <- 1000
+
+test_that("`visualize_active_subnetworks()` -- returns list of ggraph objects", {
+  # empty file case
+  empty_path <- tempfile("empty", fileext = ".txt")
+  file.create(empty_path)
+  expect_null(visualize_active_subnetworks(active_snw_path = empty_path, genes_df = input_data_frame))
+
+  skip_on_cran()
+  # default
+  g_list <- visualize_active_subnetworks(example_snw_output, input_data_frame)
+  expect_is(g_list, "list")
+  expect_is(g_list[[1]], "ggraph")
+  expect_true(length(g_list) <= example_snws_len)
+
+  # set `num_snws` to larger than actual number
+  g_list <- visualize_active_subnetworks(example_snw_output, input_data_frame,
+                                         num_snws = 21)
+  expect_is(g_list, "list")
+  expect_is(g_list[[1]], "ggraph")
+  expect_length(g_list, 13)
+})
