187b0d461826adc453493319c87f6dfbd6479390
Alem Gusinac <93720822+agusinac@users.noreply.github.com>
2026-06-22 12:31:32 +0200

    fixed matching bug, thanks to suggestion from @egeulgen
    Co-authored-by: Ege Ulgen <egeulgen@gmail.com>

--- R/visualize_term_gene_graph.R
+++ R/visualize_term_gene_graph.R
@@ -188,7 +188,7 @@ create_term_gene_graph <- function(
       # store logFC only for gene nodes
       #-----------------------------------------------------
       gene_names <- igraph::V(g)$name[cond_gene]
-      gene_logFC <- graph_df$logFC[gene_names %in% graph_df$Gene]
+      graph_df$logFC[match(gene_names, graph_df$Gene)]
 
       # Create a full-length vector with NA for term nodes
       #-----------------------------------------------------
